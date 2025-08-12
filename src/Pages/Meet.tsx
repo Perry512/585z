aefbc4e6b66c5443626de1a74a637d35a59b99b7
Perry512 <perry512@hotmail.com>
2025-08-12 14:12:16 -0400

    [FIX] Git: Fix merge conflicts probably

--- src/Pages/Meet.tsx
+++ src/Pages/Meet.tsx
@@ -4,84 +4,19 @@ import theClobberImage from '/src/assets/TheClobber.png';
 function Meet() {
     return (
         <Layout>
-            <div className="min-h-screen grid grid-rows-5 gap-4 p-4">
-                {/* Top row */}
-                <div className="grid grid-cols-4 gap-4 w-full h-40">
-                    <div className="bg-gray-600 col-span-3 flex felx-col justify-center items-around">
-                        <div className="bg-green-300 h-1/3 w-full mx-3 p-3 flex flex-row">
-                            <div className='bg-blue-100 w-full max-h-1/4'>
-                                <h1 className='text-3xl font-bold text-black'> The Clobber </h1>
-                            </div>
-                            <div> spacing </div>
-
-                        </div> 
-                        
-                    </div>
-                    <div className="bg-blue-300 col-span-1">
-                        <img src={theClobberImage}></img> 
-                    </div>
-                </div>
-
-                {/* 2nd row */}
-                <div className="grid grid-cols-4 gap-4 w-full h-40">
-                    <div className="bg-blue-300 col-span-1">
-                        <img src={theClobberImage}></img> 
-                    </div> 
-                    <div className="bg-gray-600 col-span-3 flex felx-col justify-center items-center"> 
-                        <h1 className='text-3xl font-bold text-black'> The Clobber </h1>
-                    </div> 
-                </div>
-
-                {/* 3rd row */}
-                <div className="grid grid-cols-4 gap-4 w-full h-40">
-                    <div className="bg-gray-600 col-span-3 flex felx-col justify-center items-center"> 
-                        <h1 className='text-3xl font-bold text-black'> The Clobber </h1>
-                    </div> 
-                    <div className="bg-blue-300 col-span-1">
-                        <img src={theClobberImage}></img> 
-                    </div>
-                </div>
-
-                {/* 4th row */}
-                <div className="grid grid-cols-4 gap-4 w-full h-40">
-                    <div className="bg-blue-300 col-span-1">
-                        <img src={theClobberImage}></img> 
-                    </div> 
-                    <div className="bg-gray-600 col-span-3 flex felx-col justify-center items-center"> 
-                        <h1 className='text-3xl font-bold text-black'> The Clobber </h1>
-                    </div> 
-                </div>
-
-                {/* Bottom row */}
-                <div className="grid grid-cols-4 gap-4 w-full h-40">
-                    <div className="bg-gray-600 col-span-3 flex felx-col justify-center items-center"> 
-                        <h1 className='text-3xl font-bold text-black'> The Clobber </h1>
-                    </div> 
-                    <div className="bg-blue-300 col-span-1">
-                        <img src={theClobberImage}></img> 
-                    </div>
-                </div>
-            </div>
-        </Layout>
-    )
-}
-
-export default Meet;
-import Layout from "../components/Layout";
-import theClobberImage from '/src/assets/TheClobber.png';
-
-function Meet() {
-    return (
-        <Layout>
             <div className="min-h-screen grid grid-rows-4 gap-5 p-4 bg-red-700">
                 {/* Top row */}
                 <div className="grid grid-cols-4 gap-4 w-full">
-                    <div className="bg-gray-700 col-span-3 flex felx-col justify-center items-around">
-                        <div className="h-1/3 w-full mx-3 p-3 flex flex-row">
-                            <div className='w-full max-h-1/4'>
+                    <div className="bg-gray-700 col-span-3 flex flex-col justify-start items-around h-full">
+                        <div className="h-1/5 w-full mx-3 p-3 flex flex-row bg-blue-400">
+                            <div className='w-full max-h-1/5'>
                                 <h1 className='text-3xl font-bold text-black flex flex-col justify-start items-start'> The Clobber </h1>
                             </div>
-                            <div className="max-h-1/4"></div>
+                        </div>
+                         <div className="h-1 w-11/12 bg-grey-900 "></div>
+                        <div className="h-full w-full bg-green-400 p-4">
+                            <div className="h-1/4 bg-blue-400 flex flex-col items-start translate-x-5 text-black font-bold text-xl"> Social Media Guru </div>
+                            <div className="h-3/4 bg-yellow-600 flex flex-col items-start"> <p className="text-lg"> <br/>He clobs on his ster til it's time <br/> Graduated from SUNY Brockport, Aidan enjoys spending his free time climbing, playing Tekken, Street Fighter and watching NorthernLion shorts on Youtube </p></div>
                         </div>
                     </div>
                     {/* PFP */}
