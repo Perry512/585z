cece8e25e081d26b3e2d5e02c5abf96303a44990
Perry512 <perry512@hotmail.com>
2025-08-25 20:36:32 -0400

    [ADD] Style: Spaced Contact Form closer to correct spacing

--- src/Pages/ContactFormPage.tsx
+++ src/Pages/ContactFormPage.tsx
@@ -3,13 +3,13 @@ import Layout from "../components/Layout"
 export default function ContactFormPage () {
     return(
         <Layout>
-            <div className='h-full w-full bg-amber-800'>
+            <div className='h-50vh w-screen bg-red-700 p-1 flex flex-col items-center justify-around '>
 
                 {/* Contact form header */}
-                <div className="p-5"> Contact Us!</div> 
+                <div className="p-5 bg-slate-600 w-auto mb-10"> <h1 className="font-bold text-black"> Contact Us!</h1> </div> 
 
                 { /* Contact form */}
-                <div className="py-10 flex flex-row border justify-center items-center border-black max-w-md mx-auto">
+                <div className="py-10 flex flex-row border-2 justify-center items-center border-black w-2xl max-w-md mx-auto bg-white mb-10">
                     <form>
                         <div>
                             <label htmlFor="name" className="block text-sm font-medium text-gray-700"> Name </label>
@@ -19,7 +19,7 @@ export default function ContactFormPage () {
                             <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
                             <input type="email" id="email" name="email" className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
                         </div>
-                        <div>
+                        <div className="mb-10">
                             <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label>
                             <textarea id="message" rows={4} className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 resize-none focus:ring-blue-500"></textarea>
                         </div>
