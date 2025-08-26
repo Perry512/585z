7aca8cce0cd1733446eae1b2ef250cbb175fb627
Perry512 <tnicholas512@gmail.com>
2025-08-26 01:00:02 -0400

    [ADD] Style: Added the Clobster (x3), changed layout to better suit site

--- src/Pages/Who.tsx
+++ src/Pages/Who.tsx
@@ -1,12 +1,28 @@
 import Layout from "../components/Layout";
 
 function Who() {
+
+    const discordLink = "https://media.discordapp.net/attachments/1019658872107057254/1409630053381439618/20250825_160506.jpg?ex=68ae13ca&is=68acc24a&hm=b21461db07245b2a8e7ae77b86239f9015b89c7fd88d265d8a7dbba4f07e40ca&=&format=webp&width=1032&height=688"
     return (
         <Layout>
             <div className='min-h-screen flex flex-col items-center justify-center'>
-                <h1 className='text-3xl font-bold text-white'> Who Are We? </h1>
-                <p className='text-white'> We play video games in Innonvation tower sometimes for money </p>
+                <div>
+                    <h1 className='text-3xl font-bold text-white'> Who Are We? </h1>
+                    <p className='text-white'> We play video games in Innonvation tower sometimes for money </p>
+                </div>
+                <div className="flex flex-row items-center">
+                    <div>
+                        <img src={discordLink} className="size-auto"></img>
+                    </div>
+                    <div>
+                        <img src={discordLink} className="size-auto"></img>
+                    </div>
+                    <div>
+                        <img src={discordLink} className="size-auto"></img>
+                    </div>
+                </div>
             </div>
+            
         </Layout>
     )
 }
