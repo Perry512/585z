8d0b54afacc389708c00d7258d9e2c7b4fca45f3
Perry512 <perry512@hotmail.com>
2025-08-28 11:46:17 -0400

    [ADD] Function: Clicking Site Logo now brings screen to top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,9 +9,9 @@ function Header () {
         <>
             <header className="bg-red-800 flex flex-wrap justify-between items-center w-full p-5">    
                 <div className='flex flex-row justify-center mx-10'>
-                    <a className="flex items-center">
+                    <a className="flex items-center" href="#home">
                         <img src="./src/assets/585zLogo.png" className="mr-3 h-6 sm:h-9" alt="585z Logo" />
-                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">585 FighterZ</span>
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white" id='home'>585 FighterZ</span>
                     </a>
                 </div>
                 <div className="flex flex-row justify-around">
