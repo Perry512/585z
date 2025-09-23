8f736899632d77ef165825282b9ac2dfa7773627
Perry512 <perry512@hotmail.com>
2025-09-23 18:00:41 -0400

    [ADD] Types: Created types to cleanly use components

---
+++ src/assets/photos.ts
@@ -0,0 +1,63 @@
+import type { Photo } from "../types/Photo";
+
+import TheClobber from "../assets/TheClobber.png"
+import gal1 from "../assets/gal1.jpg"
+import gal2 from "../assets/gal2.jpg"
+import gal3 from "../assets/gal3.jpg"
+import gal4 from "../assets/gal4.jpg"
+import gal5 from "../assets/gal5.jpg"
+import gal6 from "../assets/gal6.jpg"
+import gal7 from "../assets/gal7.jpg"
+
+export const photos: Photo[] = [
+    {
+        id: 1,
+        src:TheClobber,
+        alt: "The Clobber",
+        description: "The Clobster"
+    },
+    {
+        id: 2,
+        src:gal1,
+        alt: "Test",
+        description: "Test",
+    },
+    {
+        id: 3,
+        src:gal2,
+        alt: "Test",
+        description: "Test",
+    },
+    {
+        id: 4,
+        src:gal3,
+        alt: "Test",
+        description: "Test",
+    },
+    {
+        id: 5,
+        src:gal4,
+        alt: "Test",
+        description: "Test",
+    },
+    {
+        id: 6,
+        src:gal5,
+        alt: "Test",
+        description: "Test",
+    },
+    {
+        id: 7,
+        src:gal6,
+        alt: "Test",
+        description: "Test",
+    },
+    {
+        id: 8,
+        src:gal7,
+        alt: "Test",
+        description: "Test",
+    },
+
+
+]
---
+++ src/types/Person.ts
@@ -0,0 +1,10 @@
+export interface Person {
+    id: number;
+    name: string;
+    pronouns?: string;
+    role: string;
+    location: string;
+    contact?: string;
+    bio: string;
+    imageUrl: string;
+}
