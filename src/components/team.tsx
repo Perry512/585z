670c641d8cb72b3d4f91ac87dfc1149f4291187d
Perry512 <perry512@hotmail.com>
2025-09-23 18:03:03 -0400

    [ADD] Component: Created helpers to use assets

---
+++ src/components/team.tsx
@@ -0,0 +1,166 @@
+import type { Person } from "../types/Person";
+import TheClobber from "../assets/TheClobber.png";
+
+export const team: Person[] = [
+    {
+        id:1,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test1",
+        imageUrl: TheClobber
+    },
+    {
+        id:2,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test2",
+        imageUrl: TheClobber
+    },
+    {
+        id:3,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+    {
+        id:4,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+    {
+        id:5,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+        {
+        id:6,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+        {
+        id:7,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+        {
+        id:8,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+        {
+        id:9,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+        {
+        id:10,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+        {
+        id:11,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+        {
+        id:12,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+        {
+        id:13,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+        {
+        id:14,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+        {
+        id:15,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+        {
+        id:16,
+        name: "The Clobber",
+        pronouns: "He/Him",
+        role: "Media Director",
+        location: "Rochester, NY",
+        contact: "@The Clobber",
+        bio: "test test test3",
+        imageUrl: TheClobber
+    },
+
+]
