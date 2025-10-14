fc13f8c809ecaed595cb52d55de3c113cd5af54c
Perry512 <perry512@hotmail.com>
2025-10-13 21:15:48 -0400

    [FIX] Style: Corrected capitalization

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -9,7 +9,7 @@ export default function CalendarPage() {
             <div className="min-h-[90vh]">
                 <div className='min-h-[80vh] text-3xl bg-[center_top_-10rem]' style={{ backgroundImage: `url(${gal4})`}} >
                     <div className="translate-y-10">
-                        <span className="text-7xl font-bold text-black">Upcoming events</span>
+                        <span className="text-7xl font-bold text-black">Upcoming Events</span>
                     </div>
                 </div>
                 {/* Links to signups */}
