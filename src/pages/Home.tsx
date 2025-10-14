f6b66ca8e67a3883e4070034cdfeb14c19fa1af8
Perry512 <perry512@hotmail.com>
2025-10-13 22:04:24 -0400

    [FIX] Style: Adjusted coloring to backgrounds of title and home screen video

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,10 +10,10 @@ function Home() {
     return (
         <Layout>
             <div className='bg-cover bg-center h-screen max-h-screen w-full flex flex-col justify-evenly items-center bg-no-repeat bg-fixed' style={{ backgroundImage: `url(${dtr})`}}>
-                <div className='m-4 bg-slate-800 bg-opacity-90 p-5 rounded-lg'>
+                <div className='m-4 bg-zinc-800 bg-opacity-90 p-5 rounded-lg'>
                     <h1 className='text-4xl font-bold text-white'> Rochester's Newest Fighting Game Home </h1>
                 </div>
-                <div className='bg-slate-800 bg-opacity-60 rounded-lg h-1/2 w-1/2 flex flex-col justify-center items-center'>
+                <div className='bg-zinc-800 bg-opacity-60 rounded-lg h-1/2 w-1/2 flex flex-col justify-center items-center'>
                     <Suspense fallback={<p className='text-white'>Loading content...</p>}>
                         <HomePageVideo />
                     </Suspense>
