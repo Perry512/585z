e5ec610027180131759297e60b5a836dde3b4a58
Perry512 <perry512@hotmail.com>
2025-10-16 18:13:45 -0400

    [FIX] Style: Made Home frame fit Youtube landing page video

--- src/components/HomePageVideo.tsx
+++ src/components/HomePageVideo.tsx
@@ -10,12 +10,13 @@ export default function HomePageVideo() {
     if (!parent) return null;
     return(
         <div className='h-full w-full flex flex-col items-center justify-center'>
-            <iframe 
+            {/* <iframe 
                 src={`https://player.twitch.tv/?video=2547779457&parent=${parent}&muted=true&autoplay=true`}
                 height="90%"
                 width="90%"
                 allow="autoplay"
-            />
+            /> */}
+            <iframe width="728" height="409" src="https://www.youtube-nocookie.com/embed/0mNF14IH4Mg?si=IT88au97B08BG8b4&amp;start=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ function Home() {
                 <div className='m-4 bg-zinc-800 bg-opacity-90 p-5 rounded-lg'>
                     <h1 className='text-4xl font-bold text-white'> Rochester's Newest Fighting Game Home </h1>
                 </div>
-                <div className='bg-zinc-800 bg-opacity-60 rounded-lg h-1/2 w-1/2 flex flex-col justify-center items-center'>
+                <div className='bg-zinc-800 bg-opacity-60 rounded-lg h-1/2 px-10 flex flex-col justify-center items-center'>
                     <Suspense fallback={<p className='text-white'>Loading content...</p>}>
                         <HomePageVideo />
                     </Suspense>
