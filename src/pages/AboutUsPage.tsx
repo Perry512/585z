2857de90945c581c64252919424badb605d09091
Perry512 <tnicholas512@gmail.com>
2025-11-11 00:51:37 -0500

    [ADD] Component: Created more baseline code for AboutUs

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,9 +1,12 @@
+import Layout from "../components/Layout";
 import MeetV2 from "./MeetV2";
 
 export default function AboutUsPage (){
-return (
-    <>
-        <MeetV2 />
-    </>
-)
+    return (
+        <>
+            <Layout>
+                <MeetV2 />
+            </Layout>
+        </>
+    )
 }
---
+++ src/views/AboutUsStatements.tsx
@@ -0,0 +1,7 @@
+export default function AboutUsStatements() {
+    return(
+        <div>
+            <p> TEST </p>
+        </div>
+    )
+}
