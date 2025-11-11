3daeafa3a6badc4c7396119c95f5c84ae4e8e78e
Perry512 <tnicholas512@gmail.com>
2025-11-10 23:43:58 -0500

    [FIX] Component: Fixed import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import Header from './components/Header';
 import Links from './pages/Links';
 import Footer from './components/Footer';
-import AppView  from './pages/AppView';
+import AppView  from './components/AppView';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
