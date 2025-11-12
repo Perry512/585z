329a01ebc6328a82fb6bfddd68df5250ba4fa928
Perry512 <tnicholas512@gmail.com>
2025-11-12 01:52:50 -0500

    [ADD] Component: Added AboutUsStatements to AboutUsPage

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,10 +1,12 @@
 import Layout from "../components/Layout";
+import AboutUsStatements from "../views/AboutUsStatements";
 import MeetV2 from "./MeetV2";
 
 export default function AboutUsPage (){
     return (
         <>
             <Layout>
+                <AboutUsStatements />
                 <MeetV2 />
             </Layout>
         </>
