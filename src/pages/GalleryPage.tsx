f80427ccee60eb2a76dafc59a2b1ec581250f9f3
Perry512 <perry512@hotmail.com>
2025-11-11 20:18:29 -0500

    [FIX] Style: Fixed where layout component is positioned in components for consistency

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -1,9 +1,11 @@
 import PhotoGallery from "./PhotoGallery";
-
+import Layout from "../components/Layout";
 const GalleryPage = () => {
     return (
         <>
+            <Layout>
             <PhotoGallery />
+            </Layout>
         </>
     )
 }
