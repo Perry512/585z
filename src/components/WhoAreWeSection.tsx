8657c49d5698d86ce71fd7ade1ed9edb9d79a08e
Perry512 <tnicholas512@gmail.com>
2025-11-24 17:15:27 -0500

    [FIX] Asset: Added new photos to Who section, added ability to size photos

--- src/components/WhoAreWeSection.tsx
+++ src/components/WhoAreWeSection.tsx
@@ -6,19 +6,32 @@ interface WhoAreWeProps {
     headDescription: string;
     footDescription: string;
     linkTo?: string;
+    desktopPhotoSize?: string;
+    mobilePhotoSize?: string;
 }
 
-export default function WhoAreWeSection ({photo, photoAlt, headDescription, footDescription, linkTo,}: WhoAreWeProps) {
+export default function WhoAreWeSection ({
+    photo, 
+    photoAlt, 
+    headDescription, 
+    footDescription, 
+    linkTo,
+    desktopPhotoSize = "w-[25vw] h-[30vh]",
+    mobilePhotoSize = "w-full"
+}: WhoAreWeProps) {
     const content = (
-            <div className="flex flex-row h-full w-full translate-y-5 justify-center hover:border-2 hover:rounded-3xl hover:shadow-2xl">
-                <div className="flex flex-col">
-                    <div className="m-5">
+            <div className="flex flex-row h-full sm:w-[75vw] md:w-[30vw] min-h-[60vh] translate-y-5 justify-center hover:border-2 hover:rounded-3xl hover:shadow-2xl">
+                <div className="flex flex-col justify-around">
+                    <div className="flex flex-col m-5 align-top items-top">
                         <h2 className="text-3xl font-bold text-white">{headDescription}</h2>
                     </div>
-                    <div className="">
-                        <img src={photo} alt={photoAlt} className="object-contain"/>
+                    <div className="flex flex-col align-center items-center justify-center">
+                        <img 
+                            src={photo} 
+                            alt={photoAlt} 
+                            className={`object-fit ${mobilePhotoSize} md:${desktopPhotoSize}`}/>
                     </div>
-                    <div>
+                    <div className="flex flex-col align-bottom">
                         <p className="mt-5 text-center text-2xl text-white font-normal">{footDescription}</p>
                     </div>
                 </div>
--- src/pages/Who.tsx
+++ src/pages/Who.tsx
@@ -2,9 +2,9 @@ import Layout from "../components/Layout";
 import WhoAreWeSection from "../components/WhoAreWeSection";
 import { useFadeInOnScroll } from "../hooks/UseFadeInOnScroll";
 
-import asset1 from "../assets/asset1.png"
+import asset1 from "../assets/photoGal/galK.jpg"
 import asset2 from "../assets/asset2.png"
-// import asset3 from "../assets/asset3.png"
+import asset3 from "../assets/photoGal/galG.jpg"
 
 function Who() {
 
@@ -21,15 +21,32 @@ function Who() {
                         <p className='text-white'> We play video games in Innonvation tower sometimes for money </p>
                     </div>
                 </div>
-                <div className="flex flex-col items-center md:flex-row gap-5 px-10 mb-20">
+                <div className="flex flex-col items-center lg:flex-row gap-5 lg:px-10 mb-20">
                     <div ref={fadeIn1.ref} className={`${fadeIn1.className} delay-0`}>
-                        <WhoAreWeSection photo={asset1} headDescription="Section 1" footDescription="This is a long piece of text so I can test how the overflow works"/>
+                        <WhoAreWeSection 
+                            photo={asset3} 
+                            headDescription="Section 1" 
+                            footDescription="This is a long piece of text so I can test how the overflow works"
+                            desktopPhotoSize="h-[40vh] lg:w-[27vw]"
+                        />
                     </div>
                     <div ref={fadeIn2.ref} className={`${fadeIn2.className} delay-300`}>
-                        <WhoAreWeSection photo={asset2} headDescription="Photo Gallery" footDescription="See the fun and games you've missed" linkTo="/gallery"/>
+                        <WhoAreWeSection 
+                            photo={asset2} 
+                            headDescription="Photo Gallery" 
+                            footDescription="See the fun and games you've missed" 
+                            linkTo="/gallery"
+                            desktopPhotoSize="h-[45vh]"
+                        />
                     </div> 
                     <div ref={fadeIn3.ref} className={`${fadeIn3.className} delay-600`}>
-                        <WhoAreWeSection photo={asset1} headDescription="Calendar" footDescription="Find out when and where to find us" linkTo="/calendar"/>
+                        <WhoAreWeSection 
+                            photo={asset1} 
+                            headDescription="Calendar" 
+                            footDescription="Find out when and where to find us" 
+                            linkTo="/calendar"
+                            desktopPhotoSize="h-[40vh] lg:w-[27vw]"
+                        />
                     </div>
                 </div>
             </div>
