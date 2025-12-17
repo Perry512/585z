fc8c9a621ad9bc829f26c319469c26cc62559dbd
Perry512 <perry512@hotmail.com>
2025-12-17 18:58:12 -0500

    [FIX] Style: Fixed viewport of video to accomodate mobile

--- src/components/HomePageVideo.tsx
+++ src/components/HomePageVideo.tsx
@@ -9,14 +9,14 @@ export default function HomePageVideo() {
     
     if (!parent) return null;
     return(
-        <div className='h-full w-[60vw] flex flex-col items-center justify-center'>
+        <div className='sm:h-[60vh] md:h-full md:w-[60vw] flex flex-col items-center justify-center'>
             {/* <iframe 
                 src={`https://player.twitch.tv/?video=2547779457&parent=${parent}&muted=true&autoplay=true`}
                 height="90%"
                 width="90%"
                 allow="autoplay"
             /> */}
-            <iframe width="728" height="409" src="https://www.youtube-nocookie.com/embed/0mNF14IH4Mg?si=IT88au97B08BG8b4&amp;start=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/0mNF14IH4Mg?si=IT88au97B08BG8b4&amp;start=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
     )
 }
