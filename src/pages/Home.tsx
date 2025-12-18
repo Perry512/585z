e7ebedbf8adbe7991bbd0937c320f298d8873fac
Perry512 <perry512@hotmail.com>
2025-12-17 19:02:08 -0500

    [FIX] Stlye: Changed video background

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react';
-import dtr from '../assets/dtr.jpg'
+import dtr from '../assets/photoGal/galE.jpg'
 import Layout from '../components/Layout';
 
 const HomePageVideo = lazy(() => import ('../components/HomePageVideo'));
@@ -13,7 +13,7 @@ function Home() {
                 <div className='m-4 bg-zinc-800 bg-opacity-90 p-5 rounded-lg'>
                     <h1 className='text-4xl font-bold text-white'> Rochester's Newest Fighting Game Home </h1>
                 </div>
-                <div className='bg-zinc-800 bg-opacity-60 rounded-lg h-1/2 px-10 flex flex-col justify-center items-center'>
+                <div className='h-1/2 px-10 flex flex-col justify-center items-center'>
                     <Suspense fallback={<p className='text-white'>Loading content...</p>}>
                         <HomePageVideo />
                     </Suspense>
