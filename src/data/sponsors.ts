d22052a1bb6bb1e1da9b6354548bf12585982e8f
Perry512 <perry512@hotmail.com>
2026-01-17 17:21:38 -0500

    [FIX] Component: Removed sponsor, reworked how photos are imported

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -1,35 +1,24 @@
 import { Sponsor } from "../types/Sponsor";
 
-import glgLogo from "../assets/sponsorLogos/glglogo.png"
-import lGSLogo from "../assets/sponsorLogos/lGS.png"
-import theZone from "../assets/sponsorLogos/941ZoneLogo.png";
-import valhalCC from "../assets/sponsorLogos/valhalLogo.png";
-import eePathway from "../assets/sponsorLogos/eeLogo.jpeg";
-
 export const sponsors: Sponsor[] = [
     {
         name: "GLG Gaming Lounge",
-        logo: glgLogo,
+        logo: "./sponsorLogos/glglogo.png",
         link: "https://www.glgaminglounges.com/",
     },
     {
         name: "Legacy Game Store",
-        logo: lGSLogo,
+        logo: "./sponsorLogos/lGS.png",
         link: "https://legacygamestore.com/",
     },
     {
-        name: "94.1 The Zone",
-        logo: theZone,
-        link: "https://941thezone.com/",
-    },
-    {
         name: "Valhalla Combat Championship",
-        logo: valhalCC,
+        logo: "./sponsorLogos/valhalLogo.png",
         link: "https://www.vccmma.com/",
     },
     {
         name: "Exercise Express Pathways",
-        logo: eePathway,
+        logo: "./sponsorLogos/eeLogo.jpeg",
         link: "https://www.eepathways.us/",
     }
 ]
