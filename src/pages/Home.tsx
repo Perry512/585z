df2320da72ed2022a272e95a8ce36669a5e84a99
Perry512 <perry512@hotmail.com>
2026-01-17 17:24:07 -0500

    [FIX] Component: Adjested slogan, fixed photo import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react';
-import dtr from '../assets/photoGal/galE.jpg'
+import barBackground from '/photoGal/galE.jpg'
 import Layout from '../components/Layout';
 
 const HomePageVideo = lazy(() => import ('../components/HomePageVideo'));
@@ -9,9 +9,9 @@ function Home() {
 
     return (
         <Layout>
-            <div className='bg-cover bg-center h-screen max-h-screen w-full flex flex-col justify-evenly items-center bg-no-repeat bg-fixed' style={{ backgroundImage: `url(${dtr})`}}>
+            <div className='bg-cover bg-center h-screen max-h-screen w-full flex flex-col justify-evenly items-center bg-no-repeat bg-fixed' style={{ backgroundImage: `url(${barBackground})`}}>
                 <div className='m-4 bg-zinc-800 bg-opacity-90 p-5 rounded-lg'>
-                    <h1 className='text-4xl font-bold text-white'> Rochester's Newest Fighting Game Home </h1>
+                    <h1 className='text-4xl font-bold text-white'> Rochester's Premiere Fighting Game Scene </h1>
                 </div>
                 <div className='h-1/2 px-10 flex flex-col justify-center items-center'>
                     <Suspense fallback={<p className='text-white'>Loading content...</p>}>
