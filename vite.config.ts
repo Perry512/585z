a8eb8935031c82516b8809f752bf74ea84d1df4d
Perry512 <perry512@hotmail.com>
2026-02-05 15:45:39 -0500

    [FIX] Function: Changed Base to help vercel transfer

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/585z/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
