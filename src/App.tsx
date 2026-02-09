8bfedbf9c6c937ff4dfea4ddaf831bca6c3ed159
Perry512 <tnicholas512@gmail.com>
2026-02-08 19:36:37 -0500

    [FIX] Function: Switched to HashRouter from Router as a bandaid fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,20 @@ import './App.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import AppView  from './components/AppView';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import LargeSocialLinkBanner from './components/LargeSocialLinkBanner';
 
 function App() {
 
   return (
-    <BrowserRouter basename="/585z">
+    <HashRouter>
         <Header />
         <AppView />
         <LargeSocialLinkBanner />
         <Footer />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
-
+//         <Route path="/" element={<Home />} />
+//       <Route path="/calendar" element={<Calendar />} />
 export default App;
